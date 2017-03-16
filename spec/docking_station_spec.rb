42007ae1414a47d8a93df48467864d4fa6fa9414
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-03-16 11:01:37 +0000

    adds unit tests and implements guard condition to catch errors

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,29 @@
+require 'docking_station'
+
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike) }
+
+#it 'releases working bikes' do
+#    bike = subject.release_bike
+#   expect(bike).to be_working
+ #end
+
+  it 'docks bikes' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  describe '#release_bike' do
+  it 'raises an error when there are no bikes available' do
+    expect { subject.release_bike}.to raise_error 'No bikes available'
+  end
+ end
+end
